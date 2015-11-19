d0bbbc6e1e7603e665d10a8f0dbcd6924c2c2b07
Lingbin Cai <lingbinc@seas.upenn.edu>
2015-11-18 21:51:07 -0500

    url utils

---
+++ UrlUtils.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults  #-}
+module UrlUtils where
+
+getDomain :: String -> String
+getDomain = undefined
+
+getRelativePath :: String -> String
+getRelativePath = undefined
+
+getType :: String -> String
+getType = undefined
+
+matchPath :: String -> String -> Boolean
+matchPath = undefined
