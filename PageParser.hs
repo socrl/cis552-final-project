739db2779ac06bd023fd5110d8d199a67bef6b37
Kathy <kchen2013@gmail.com>
2015-11-27 12:02:35 -0500

    started adding some test strings, implemented the bulk of the robots.txt parser for a general, well-formed textfile.

--- PageParser.hs
+++ PageParser.hs
@@ -1,5 +1,4 @@
 {-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults  #-}
-{-# LANGUAGE FlexibleInstances #-}
 
 module PageParser where
 import qualified Parser as P
@@ -7,17 +6,20 @@ import qualified ParserCombinators as P
 
 import Control.Applicative (Alternative(..))
 
-type Robot = ([RelPath], Int)
+type Robot = ([LineInfo], Int)
 type RelPath = String
-
+data LineInfo =
+    Allow RelPath
+  | Disallow RelPath
+  | Comment 
+  | CrawlDelay Int 
+  deriving (Eq, Show)
 
 getSnips :: String -> String -> [String]
 getSnips = undefined
 
 parseRobot :: String -> Robot
-parseRobot _ = undefined {-retrieval $ lines s where
-	retrieval []     = ([], 0)
-	retrieval (l:ls) = case words l of-} 
+parseRobot s = undefined --P.doParse $ many (notOurUserAgentP <|> userAgentP)
 
 -- | Applies a parser and then skips over any whitespace directly after it
 wsP :: P.Parser a -> P.Parser a
@@ -25,28 +27,41 @@ wsP p = do n <- p
            _ <- many P.space             
            return n
 
-disallowUrlP :: P.Parser (String, Bool) 
-disallowUrlP = do _ <- wsP $ P.string "Disallow:"
-                  l <- relUrlP 
-                  _ <- wsP $ untilEOL
-                  return (l, False) 
-
-allowUrlP :: P.Parser (String, Bool) 
-allowUrlP = do _ <- wsP $ P.string "Allow:"
-               l <- relUrlP 
-               _ <- wsP $ untilEOL
-               return (l, True) 
-
-userAgentP :: P.Parser [(String, Bool)]
+userAgentP :: P.Parser [LineInfo]
 userAgentP = do _ <- many commentP
                 _ <- wsP $ P.string "User-agent:"
                 _ <- wsP $ P.char '*'
-                out <- many (allowUrlP <|> disallowUrlP)
+                out <- lineInfoP
                 return out
 
-newline :: P.Parser () 
-newline = do _ <- wsP $ P.char '\n'
-             return ()
+notOurUserAgentP :: P.Parser [LineInfo]
+notOurUserAgentP = do _ <- many commentP
+                      _ <- wsP $ P.string "User-agent:"
+                      _ <- P.satisfy (/= '*')
+                      _ <- wsP $ untilEOL
+                      _ <- lineInfoP
+                      return []
+
+lineInfoP :: P.Parser [LineInfo]
+lineInfoP = many (allowUrlP <|> disallowUrlP <|> crawlDelayP <|> commentP)
+
+disallowUrlP :: P.Parser LineInfo 
+disallowUrlP = do _ <- wsP $ P.string "Disallow:"
+                  l <- relUrlP 
+                  _ <- wsP $ untilEOL
+                  return $ Disallow l 
+
+allowUrlP :: P.Parser LineInfo 
+allowUrlP = do _ <- wsP $ P.string "Allow:"
+               l <- relUrlP 
+               _ <- wsP $ untilEOL
+               return $ Allow l 
+
+crawlDelayP :: P.Parser LineInfo
+crawlDelayP = do _ <- wsP $ P.string "Crawl-delay:"
+                 n <- P.int 
+                 _ <- wsP $ untilEOL
+                 return $ CrawlDelay n 
 
 untilEOL :: P.Parser String
 untilEOL = many $ P.satisfy (/= '\n')
@@ -54,8 +69,7 @@ untilEOL = many $ P.satisfy (/= '\n')
 relUrlP :: P.Parser String
 relUrlP = many $ P.satisfy (\x -> x /= '\n' && x /= '#')
 
-commentP :: P.Parser String
+commentP :: P.Parser LineInfo
 commentP = do _ <- P.char '#'
-              c <- untilEOL
-              _ <- newline
-              return c
+              _ <- wsP $ untilEOL
+              return Comment
--- Tests.hs
+++ Tests.hs
@@ -1,2 +1,32 @@
 {-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults  #-}
 module Tests where
+
+import Test.HUnit (runTestTT,Test(..),Assertion, (~?=), (~:), assert)
+import Test.QuickCheck (Arbitrary(..), Testable(..), Gen, elements, 
+  oneof, frequency, sized, quickCheckWith, stdArgs, maxSize, maxSuccess)
+
+import qualified PageParser as P
+import qualified Parser as P
+import qualified ParserCombinators as P
+
+comment :: String
+comment = "# robots.txt for http://www.wikipedia.org/ and friends\n"
+
+multilineComment :: String
+multilineComment = "#\n# robots.txt for http://www.wikipedia.org/ and friends\n#\n# Please note: There are a lot of pages on this site, and there are\n# some misbehaved spiders out there that go _way_ too fast. If you're\n# irresponsible, your access to the site may be blocked.\n#\n\n# advertising-related bots:\n"
+
+notOurUserAgent :: String
+notOurUserAgent = "User-agent: Mediapartners-Google*\nDisallow: /\n"
+
+agentCommentMix :: String
+agentCommentMix = multilineComment ++ notOutUserAgent ++ "\n# Wikipedia work bots:\nUser-agent: *\nDisallow:\n"
+
+validUserAgent :: String 
+validUserAgent = "User-agent: *\nDisallow: /search\nAllow: /search/about\nDisallow: /sdch\nDisallow: /groups\nDisallow: /catalogs\nAllow: /catalogs/about\nAllow: /catalogs/p?\nDisallow: /catalogues\nAllow: /newsalerts\nDisallow: /news\nAllow: /news/directory\nDisallow: /nwshp\nDisallow: /setnewsprefs?\nDisallow: /index.html?\nDisallow: /?\nAllow: /?hl=\nDisallow: /?hl=*&\nAllow: /?hl=*&gws_rd=ssl$\nDisallow: /?hl=*&*&gws_rd=ssl\nAllow: /?gws_rd=ssl$\nAllow: /?pt1=true$\nDisallow: /addurl/image?\nAllow:    /mail/help/\nDisallow: /mail/\nDisallow: /pagead/\nDisallow: /relpage/\nDisallow: /relcontent\nDisallow: /imgres\nDisallow: /imglanding\nDisallow: /sbd\nDisallow: /keyword/\nDisallow: /u/\nDisallow: /univ/\nDisallow: /cobrand\nDisallow: /custom\nDisallow: /advanced_group_search\nDisallow: /googlesite\nDisallow: /preferences\nDisallow: /setprefs\nDisallow: /swr\nDisallow: /url\nDisallow: /default\nDisallow: /m?\nDisallow: /m/\nAllow:    /m/finance\nDisallow: /wml?\nDisallow: /wml/?\nDisallow: /wml/search?\nDisallow: /xhtml?\nDisallow: /xhtml/?\nDisallow: /xhtml/search?\nDisallow: /xml?\nDisallow: /imode?\nDisallow: /imode/?\nDisallow: /imode/search?\nDisallow: /jsky?\nDisallow: /jsky/?\nDisallow: /jsky/search?\nDisallow: /pda?\nDisallow: /pda/?\nDisallow: /pda/search?\nDisallow: /sprint_xhtml\nDisallow: /sprint_wml\nDisallow: /pqa\nDisallow: /palm\nDisallow: /gwt/\nDisallow: /purchases\nDisallow: /bsd?\nDisallow: /linux?\nDisallow: /mac?\nDisallow: /microsoft?\nDisallow: /unclesam?\nDisallow: /answers/search?q=\nDisallow: /local?\nDisallow: /local_url\nDisallow: /shihui?\nDisallow: /shihui/\nDisallow: /froogle?\nDisallow: /products?\nDisallow: /froogle_\nDisallow: /product_\nDisallow: /products_\nDisallow: /products;\nDisallow: /print\nDisallow: /books/\nDisallow: /bkshp?*q=*\nDisallow: /books?*q=*\nDisallow: /books?*output=*\nDisallow: /books?*pg=*\nDisallow: /books?*jtp=*\n"
+
+userAgentInlineComment :: String
+userAgentInlineComment = "User-agent: *\nDisallow: /search\nAllow: /search/about # COMMENTARY\nDisallow: /sdch\n"
+
+validRobotsPg :: String
+validRobotsPg = multilineComment ++ notOurUserAgent ++ "\nUser-agent: Orthogaffe\nDisallow:\n" ++ comment ++ validUserAgent ++ comment ++ "Disallow: /wiki/Wikipedia:Strony_do_usuni%C4%99cia\n"
+
