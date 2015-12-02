182a94e746f7fd8118fc5e6a28fba8aa3f2cd538
Lingbin Cai <lingbinc@seas.upenn.edu>
2015-12-02 14:52:29 -0500

    crawling check

--- Downloader.hs
+++ Downloader.hs
@@ -10,42 +10,94 @@ import PageParser
 
 import Control.Monad
 import Data.Time.Clock
-import Data.HashSet
 import Data.Maybe
 import Control.Monad.Trans.Maybe
 import Control.Monad.State
 import Control.Monad.IO.Class
 import Text.HandsomeSoup
 
+import Data.HashSet
+
 import Data.Map (Map)
 import qualified Data.Map as Map
 
-import State (State)
-import qualified State as S
-
-type Result = (String, String)
+type Result = (String, [String])
 
 type Frontier   = Queue String
 type Visited    = HashSet String
-type ServerInfo = Map String Robot
+type ServerInfo = Map String (Robot, UTCTime)
 
 type Status = (Frontier, Visited, ServerInfo, [Result])
 
-sendReqs :: String -> String -> Int -> Either [Result] String
+sendReqs :: String -> String -> Int -> Either (IO [Result]) String
 sendReqs = undefined
 
-execute :: (MonadState Status m) => Int -> Int -> m ()
-execute ord lim | ord > lim = return ()
-				        | otherwise = do 
-  (f, v, s, rl) <- S.get
+execute :: String -> Int -> Int -> StateT Status IO ()
+execute query ord lim | ord > lim = return ()
+                      | otherwise = do 
+  (f, v, s, rl) <- get
   case dequeue f of 
-  	 Nothing        -> return ()
-  	 Just (url, f') -> case getDomain url >>= (\host -> Map.lookup host s) of
-                         Nothing -> do contents <- (runMaybeT $ openUrl ("http://" ++ domain ++ "/robots.txt"))
-                                       S.put (f, v, Map.insert domain (parseRobot $ fromJust contents) s, rl)
-                                       execute ord lim where
-                                         domain = fromJust $ getDomain url
-      	                 Just (lst_v, freq) -> undefined 
+     Nothing        -> return ()
+     Just (url, f') -> do
+      -- check robots information
+      case getDomain url >>= (\host -> Map.lookup host s) of
+        Nothing -> do 
+          -- robots info hasn't been retrieved yet
+          let domain = fromJust $ getDomain url
+          contents <- fetchContents ("http://" ++ domain ++ "/robots.txt")
+          case contents of
+            Nothing  -> return ()
+            Just con -> do currT <- liftIO getCurrentTime
+                           put (f, v, Map.insert domain 
+                                ((parseRobot $ fromJust contents), currT) s, rl)
+                           execute query ord lim
+        Just (robot, l_vt) -> do
+          -- contain robots info
+          let pass = canCrawl robot l_vt url 
+          -- cannot crawl, drop this url
+          if pass < 0 then do
+            put (f', v, s, rl)
+            execute query ord lim
+          -- can crawl, but need delay
+          else if pass == 0 then do
+            put (enqueue url f', v, s, rl)
+            execute query ord lim
+          -- can crawl right now
+          else do
+            contents <- fectchContents url 
+            case contents >>= (Just . (getSnips query)) of
+              Nothing -> do put (f', v, s, rl)
+                            execute query (ord+1) lim
+              Just _ ->  undefined
+
+
+fetchContents :: (MonadIO m) => String -> m (Maybe String)
+fetchContents = liftIO . runMaybeT . openUrl
+
+-- check if the url can be crawled, 
+-- 0  - can crawl but need wait; 
+-- 1  - can crawl right now; 
+-- -1 - cannot crawl
+canCrawl :: (MonadIO m) => Robot -> UTCTime -> String -> m Int
+canCrawl (ll, c_del) l_vt url = do 
+  if not $ pathAllow ll url then return (-1)
+    else do
+      currT <- liftIO getCurrentTime
+      if (diffUTCTime currT l_vt < c_del) then return 0
+        else return 1
+
+-- Check if the url is allowed to crawl
+pathAllow :: [LineInfo] -> String -> Bool
+pathAllow ll url = Prelude.foldr (&&) True $ Prelude.map (lineCheck url) ll
+
+-- Check if the line info is compatible with a url
+lineCheck :: String -> LineInfo -> Bool
+lineCheck url li = 
+  case li of
+    Allow    p   -> matchPath url p
+    Disallow p   -> not $ matchPath url p
+    Comment      -> True
+    CrawlDelay _ -> True
 
 
 {-
