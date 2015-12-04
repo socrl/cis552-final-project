8b842b4e4819e41dabd550a4832dd3bafd411e07
Lingbin Cai <lingbinc@seas.upenn.edu>
2015-12-03 23:27:13 -0500

    can crawl more pages now

--- Downloader.hs
+++ Downloader.hs
@@ -131,23 +131,3 @@ lineCheck url li =
     Comment      -> True
     CrawlDelay _ -> True
 
-
-{-
-1. receive URL, search string, num pages from Main
-
-2. put input URL in queue, do the initialization stuff (map <server, <last visit time, frequency>, queue <url> - the frontier, hashset <url> - did we visit this webpage)
-    when we get things off the queue, if we haven't got the site's robots.txt in the map, we request it
-
-3. perform requests
-    a. get from queue
-    b. check for that url's server - is that in the map
-        if not, get its robots.txt
-    c. make a request to get the text of that page
-    d. tell parser to get snippets
-    e. store the tuples of (URL, snippets)
-    f. update 3 data structures as necessary
-
-4. return the (URL, snippets)
-
--}
-
