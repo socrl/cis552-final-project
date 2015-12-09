0fd4be8327b60770c17d9b3b6ce7bb366150429b
socrl <slsocrli@gmail.com>
2015-12-09 01:58:48 -0500

    working on computing distance

--- PostProcessor.hs
+++ PostProcessor.hs
@@ -5,17 +5,13 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.String.Utils
 import Data.Char
+import Data.List
+import Data.Ord
 
--- | given a string and a list of keywords, find the indices at which each
--- keyword occurs
-findWords :: String -> [String] -> Map String [Int]
-findWords doc ks =
-  findWordsHelp
-    (filter (not . null) (map (strLower . trimNonAlpha) (splitWs doc)))
-    (map (strLower . trimNonAlpha) ks)
-
-findWordsHelp :: [String] -> [String] -> Map String [Int]
-findWordsHelp = f Map.empty 0 where
+-- | Given a list of words and a list of keywords, find the indices at which
+-- each keyword occurs
+findWords :: [String] -> [String] -> Map String [Int]
+findWords = f Map.empty 0 where
   f m i (w:ws) ks =
     if elem w ks then
       case Map.lookup w m of
@@ -33,3 +29,52 @@ strLower = foldr (\ c st -> (toLower c):st) ""
 trimNonAlpha :: String -> String
 trimNonAlpha s = reverse $ f $ reverse $ f s where
   f = dropWhile (not . isAlphaNum)
+
+-- | rank the texts by their relevance to the keywords, in descending order
+rankPages :: [(String, String)] -> [String] -> [(String, String, Int)]
+rankPages pgs keys = sortBy (flip $ comparing (\ (_, _, x) -> x))
+  (map (getPgValue (keyFormat keys)) pgs)
+
+-- | given the desired keywords and a page, get an integer representing its
+-- worth
+getPgValue :: [String] -> (String, String) -> (String, String, Int)
+getPgValue keys (url, txt) = (url, txt, val) where
+  m = findWords (txtFormat txt) keys
+  val = (numOccur m) * (numDistinct m) `div` if avgD > 0 then avgD else 1
+  avgD = avgDist m keys
+
+-- | given a map of occurrences, determine the number of times any keyword
+-- occurred
+numOccur :: Map String [Int] -> Int
+numOccur m = foldr (\ x accu -> length x + accu) 0 (Map.elems m)
+
+-- | given a map of occurrences, determine the number of distinct keywords that
+-- occurred
+numDistinct :: Map String [Int] -> Int
+numDistinct = length . Map.keys
+
+-- | given a map of occurrences, determine the average distance between
+-- occurrences of keywords, ignoring the bodies of the keywords themselves
+avgDist :: Map String [Int] -> [String] -> Int
+avgDist m keys = undefined
+
+
+
+
+
+
+
+
+
+
+-- | formatting the text from the webpage: split text on whitespace, trim
+-- non-alphanumeric characters, convert to lowercase, remove empty strings
+txtFormat :: String -> [String]
+txtFormat doc =
+  filter (not . null) (map (strLower . trimNonAlpha) (splitWs doc))
+
+-- | formatting the keywords: trim non-alphanumeric characters, convert to
+-- lowercase, remove empty strings
+keyFormat :: [String] -> [String]
+keyFormat k =
+  filter (not . null) (foldr (\ s st -> (strLower $ trimNonAlpha s):st) [] k)
