b2df601ec3c41bab38c7631e0979a249245d8879
Lingbin Cai <lingbinc@seas.upenn.edu>
2015-12-10 17:19:09 -0500

    refactoring

--- Downloader.hs
+++ Downloader.hs
@@ -28,13 +28,19 @@ type Frontier   = Queue String
 type Visited    = HashSet String
 type ServerInfo = Map String (Robot, UTCTime)
 
+type URL        = String
+type Query      = String
+
+data CrawlCheck = NoCrawl
+                | CanCrawl
+                | NeedWait
+
 -- Status maintains four states
 -- 1. Frontier  : storing the URLs to be crawled
 -- 2. Visited   : the URLs that have been crawled
 -- 3. ServerInfo: mapping the domain to the robots information of the 
 --                domain, and last visit time
--- 4. [Result]  : list of results, including the URL and a list of 
---                matched snippets
+-- 4. [Result]  : list of results, including the URL and the HTML doc
 type Status = (Frontier, Visited, ServerInfo, [Result])
 
 allowedTypes :: [String]
@@ -42,7 +48,7 @@ allowedTypes = ["html", "php", "txt"]
 
 -- API for top level function, pass in a starting URL address,
 -- a search query and upper limit of # of pages to crawl
-sendReqs :: String -> String -> Int -> IO ([Result], Int)
+sendReqs :: URL -> Query -> Int -> IO ([Result], Int)
 sendReqs url query lim = do 
   (_, (_, v, _, rl)) <- runStateT (execute query 0 lim) 
                         (single url, Hashset.empty, Map.empty, [])
@@ -50,10 +56,11 @@ sendReqs url query lim = do
 
 -- Main execution method, responsible for scheduling tasks, sending
 -- HTTP request, pass web content to parser and storing results 
-execute :: String -> Int -> Int -> StateT Status IO ()
+execute :: Query -> Int -> Int -> StateT Status IO ()
 execute query ord lim | ord >= lim = return ()
                       | otherwise  = do 
-  (f, v, s, rl) <- get
+  status@(f, v, s, rl) <- get
+  currT <- liftIO getCurrentTime
   case dequeue f of 
      Nothing        -> return ()
      Just (url, f') -> do
@@ -61,69 +68,83 @@ execute query ord lim | ord >= lim = return ()
       case getDomain url >>= (\host -> Map.lookup host s) of
         Nothing -> do 
           -- robots info hasn't been retrieved yet
-          let domain = fromJust $ getDomain url
-          contents <- fetchContents ("http://" ++ domain ++ "/robots.txt")
-          case contents of
-            Nothing  -> return ()
-            Just con -> do currT <- liftIO getCurrentTime
-                           put (f, v, Map.insert domain 
-                                ((parseRobot con), currT) s, rl)
-                           execute query ord lim
-        Just (robot, l_vt) -> do
-          -- contain robots info
-          pass <- canCrawl robot l_vt url 
-          -- cannot crawl, drop this url
-          if pass < 0 then do
-            put (f', v, s, rl)
-            execute query ord lim
-          -- can crawl, but need delay
-          else if pass == 0 then
-            execute query ord lim
-          -- can crawl right now
-          else do
-            contents <- fetchContents url 
-            case contents >>= (Just . (parseDoc url query)) of
-              Nothing  -> do 
-                put (f', v, s, rl)
-                execute query (ord+1) lim
-              Just result -> do 
-                (urls, doc) <- liftIO result
-                let v'  = Hashset.insert url v
-                    f'' = Prelude.foldr (\u front -> enqueue u front) f' urls
-                put (f'', v', s, (url, doc) : rl)
-                execute query (ord+1) lim
+          getRobot status url currT
+          execute query ord lim
+        Just (robot, lastT) -> do
+          -- contain robots info, try crawl the web
+          let check = canCrawl robot lastT currT url 
+          case check of
+            NoCrawl  -> do
+              put (f', v, s, rl)
+              execute query ord lim
+            NeedWait -> execute query ord lim
+            CanCrawl -> do
+              getContentAndSearch status url query f'
+              execute query (ord+1) lim
 
 
-fetchContents :: (MonadIO m) => String -> m (Maybe String)
+-- helper function for scheduler that fetch the robots.txt info
+getRobot :: Status -> URL -> UTCTime -> StateT Status IO ()
+getRobot status url currT = do
+  let (f, v, s, rl) = status
+      domain = fromJust $ getDomain url
+  contents <- fetchContents ("http://" ++ domain ++ "/robots.txt")
+  case contents of
+    Nothing  -> return ()
+    Just con -> put (f, v, Map.insert domain 
+                     ((parseRobot con), currT) s, rl)
+                   
+-- helper function for scheduler that fetch the web page, parse and search
+-- for keywords
+getContentAndSearch :: Status -> URL -> Query -> Frontier -> StateT Status IO ()
+getContentAndSearch status url query f' = do
+  let (_, v, s, rl) = status
+  contents <- fetchContents url 
+  -- parse the document to get contained URLs and see if any query word matched
+  case contents >>= (Just . (parseDoc url query)) of
+    Nothing  -> put (f', v, s, rl)
+    Just result -> do 
+      (urls, doc) <- liftIO result
+      let v'  = Hashset.insert url v
+          f'' = Prelude.foldr (\u front -> enqueue u front) f' urls
+      if Prelude.null doc then
+        put (f'', v', s, rl)
+      else
+        put (f'', v', s, (url, doc) : rl)
+
+-- helper function that fetch the contents from an URL
+fetchContents :: (MonadIO m) => URL -> m (Maybe String)
 fetchContents url = liftIO $ catch ((liftIO . runMaybeT . openUrl) url) 
                                    (\e -> return Nothing `const` 
                                           (e :: IOException))
 
--- check if the url can be crawled, 
--- 0  - can crawl but need wait; 
--- 1  - can crawl right now; 
--- -1 - cannot crawl
-canCrawl :: (MonadIO m) => Robot -> UTCTime -> String -> m Int
-canCrawl (ll, delay) last_vt url = do 
-  if not (pathAllow ll url && typeCheck url) then return (-1)
-    else do
-      currT <- liftIO getCurrentTime
-      -- check if enough time has passed since last site visit
-      if ((realToFrac $ diffUTCTime currT last_vt :: Double) < 
-          (fromIntegral delay :: Double)) then return 0 
-      else return 1
+-------------- helper functions that do not require IO -----------------
 
-typeCheck :: String -> Bool
-typeCheck url = case getType url >>= \x -> find (x ==) allowedTypes of
+-- check if the url can be crawled with three possible states
+canCrawl :: Robot -> UTCTime -> UTCTime -> URL -> CrawlCheck
+canCrawl (ll, delay) lastT currT url = 
+  if not (pathAllow ll url && typeAllow url) then NoCrawl
+    else if timeAllow lastT currT delay then NeedWait
+      else CanCrawl
+
+-- Check if the URL has proper type for crawling
+typeAllow :: URL -> Bool
+typeAllow url = case getType url >>= \x -> find (x ==) allowedTypes of
                   Nothing -> False
                   Just _  -> True
 
--- Check if the url is allowed to crawl
-pathAllow :: [LineInfo] -> String -> Bool
+-- Check if enough time has passed since last crawl
+timeAllow :: UTCTime -> UTCTime -> Int -> Bool
+timeAllow lastT currT delay = 
+  (realToFrac $ diffUTCTime currT lastT :: Double) < 
+  (fromIntegral delay :: Double)
+
+-- Check if the url is allowed to crawl based on path info from Robots.txt
+pathAllow :: [LineInfo] -> URL -> Bool
 pathAllow ll url = Prelude.foldr (&&) True $ Prelude.map (lineCheck url) ll
 
 -- Check if the line info is compatible with a url
-lineCheck :: String -> LineInfo -> Bool
+lineCheck :: URL -> LineInfo -> Bool
 lineCheck url li = 
   case li of
     Allow    p   -> matchPath url p
